ad6657dc1531d68dc5efc22daac2dc85e33f74f7
luopan <lxpanup@163.com>
2018-12-05 22:44:14 +0800

    上传springcloud

---
+++ modules/eureka-client/src/main/java/com/lxp/client/EurekaClientApplication.java
@@ -0,0 +1,14 @@
+package com.lxp.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaClientApplication.class, args);
+	}
+}
